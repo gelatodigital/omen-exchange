7b90466f38a6381f3201b16668b275c2743f1465
Hilmar X <x@hilmarx.com>
2020-10-30 09:42:50 +0100

    deleted changes to origin master

--- app/src/components/market/sections/market_create/market_wizard_creator_container.tsx
+++ app/src/components/market/sections/market_create/market_wizard_creator_container.tsx
@@ -38,7 +38,7 @@ const MarketWizardCreatorContainer: FC = () => {
           return
         }
 
-        // @dev Change Modal to Gelato Conditioned
+        // @dev Change Modal to Gelato Condition
         setMarketCreationStatus(MarketCreationStatus.creatingAMarket())
 
         if (!cpk.cpk.isSafeApp()) {
