565c6132fd96e5fa3693cb158e3a967bde6f5ad5
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2020-11-08 11:04:24 +0100

    Remove trailing curly brace

--- app/src/queries/gelato.ts
+++ app/src/queries/gelato.ts
@@ -4,40 +4,39 @@ export const GelatoSubmitted = gql`
   query taskReceiptWrappers($user: String!) {
     taskReceiptWrappers(where: { user: $user }) {
       taskReceipt {
-          id
-          userProxy
-          provider {
-            addr
-            module
-          }
-          index
-          tasks {
-            conditions {
-              inst
-              data
-            }
-            actions {
-              addr
-              data
-              operation
-              dataFlow
-              value
-              termsOkCheck
-            }
-            selfProviderGasLimit
-            selfProviderGasPriceCeil
-          }
-          expiryDate
-          cycleId
-          submissionsLeft
+        id
+        userProxy
+        provider {
+          addr
+          module
         }
-        submissionHash
-        status
-        submissionDate
-        executionDate
-        executionHash
-        selfProvided
+        index
+        tasks {
+          conditions {
+            inst
+            data
+          }
+          actions {
+            addr
+            data
+            operation
+            dataFlow
+            value
+            termsOkCheck
+          }
+          selfProviderGasLimit
+          selfProviderGasPriceCeil
+        }
+        expiryDate
+        cycleId
+        submissionsLeft
       }
+      submissionHash
+      status
+      submissionDate
+      executionDate
+      executionHash
+      selfProvided
     }
   }
 `
